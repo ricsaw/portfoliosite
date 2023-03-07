538cf453306cc909f5e8388e64a3df87ec81c301
ras7019 <46506471+q1king@users.noreply.github.com>
2023-03-07 00:55:59 -0500

    added resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,16 @@
-import resume from "../pages/about/Richard-Sawh-Resume.pdf";
+import resume from "../pages/resume/Richard-Sawh-Resume.pdf";
 import React from 'react';
+import { Document, Page } from 'react-pdf';
 
+//show the resume pdf
 const Resume = () => {
-    //show the resume pdf
     return (
-        <div className="resume">
-            <iframe src={resume} title="resume" width="100%" height="100%"></iframe>
+        <div className="resumeContainer">
+            <Document file={resume}>
+                <Page pageNumber={1} />
+            </Document>
         </div>
     );
-};
+}
 
 export default Resume;
