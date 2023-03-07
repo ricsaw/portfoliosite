f54a6489921872e5e3aad6357fcd57a97094a15a
ras7019 <46506471+q1king@users.noreply.github.com>
2023-03-07 01:03:24 -0500

    added resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,25 @@
-import resume from "../pages/resume/Richard-Sawh-Resume.pdf";
+// import resume from "../pages/resume/Richard-Sawh-Resume.pdf";
 import React from 'react';
 import { Document, Page } from 'react-pdf';
 
-//displau the resume in a pdf viewer
-const Resume = () => {
+function Resume() {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+  
+    function onDocumentLoadSuccess({ numPages }) {
+      setNumPages(numPages);
+    }
+  
     return (
-        <div className="resumePage">
-            <Document file={resume}>
-                <Page pageNumber={1} />
-            </Document>
-        </div>
+      <div>
+        <Document file="/pages/resume/Richard-Sawh-Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
     );
-}
+  }
 
 export default Resume;
