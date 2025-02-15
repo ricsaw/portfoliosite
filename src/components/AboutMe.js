353406a21568e451ad3a48634ab3926262ce8b7b
ras7019 <46506471+q1king@users.noreply.github.com>
2025-02-15 18:11:34 -0500

    aboutme page update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,4 @@
-// AboutMe.js
+// import aboutMeImg from "../images/aboutme.jpg";
 import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
@@ -7,46 +7,83 @@ import resume from "../pages/about/Richard-Sawh-Resume.pdf";
 import React from 'react';
 
 const AboutMe = ({ name, email, location, availability, brand }) => {
-  const [ref, inView] = useInView({ threshold: 0.2, triggerOnce: true });
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
   const [downloading, setDownloading] = useState(false);
 
-  useEffect(() => setDownloading(false), [downloading]);
+  useEffect(() => {
+    setDownloading(false);
+  }, [downloading]);
 
   const handleDownload = () => {
     setDownloading(true);
     const link = document.createElement("a");
     link.href = resume;
     link.download = "Richard-Sawh-Resume.pdf";
+    link.onload = () => {
+      link.remove();
+      setDownloading(false);
+    };
     document.body.appendChild(link);
     link.click();
-    document.body.removeChild(link);
-    setDownloading(false);
   };
 
   return (
     <div className="aboutContainer container">
-      <div className="row center-content">
+      <div className="row" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        {/* <motion.div
+          className="personalImage col-12 col-lg-4"
+          ref={ref}
+          initial={{ x: "-10vw", opacity: 0 }}
+          animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <img src={aboutMeImg} alt={name} />
+        </motion.div> */}
         <motion.div
           className="personalInfo col-12 col-lg-8"
           ref={ref}
           initial={{ x: "10vw", opacity: 0 }}
           animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           <div className="contentContainer">
             <h4>Greetings</h4>
             <h5>Full Stack Developer</h5>
-            <p className="contentDescription">{brand}</p>
+            <div className="contentDescription">
+              <p>{brand}</p>
+            </div>
             <div className="infoContainer">
-              <div className="info"><span>Name:</span> <p>{name}</p></div>
-              <div className="info"><span>Email:</span> <p><a href={`mailto:${email}`}>{email}</a></p></div>
-              <div className="info"><span>Location:</span> <p>{location}</p></div>
-              <div className="info"><span>Availability:</span> <p>{availability}</p></div>
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Name:</span>
+                  <p>Richard Sawh</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Email:</span>
+                  <p>
+                    <a href={`mailto:${email}`}>{email}</a>
+                  </p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Location:</span>
+                  <p>{location}</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Availability:</span>
+                  <p>{availability}</p>
+                </div>
+              </div>
             </div>
             <div className="buttonContainer">
               <button className="btn downloadCV" onClick={handleDownload} disabled={downloading}>
                 {downloading ? "Downloading..." : "Download Resume"}
-              </button>
+              </button>{" "}
               <SocialIcons />
             </div>
           </div>
